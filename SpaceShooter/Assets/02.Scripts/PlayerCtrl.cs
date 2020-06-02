d12eb0b4560d62d6f4f1a9e27aea2f962d275443
kamitory <necohoosen@gmail.com>
2020-06-02 11:50:09 +0900

    no message

---
+++ SpaceShooter/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Debug.Log("h =" + h.ToString());
+        Debug.Log("v =" + v.ToString());
+
+        tr.Translate (Vector3.forward * moveSpeed * v * Time.deltaTime,Space.Self);
+    }
+}
